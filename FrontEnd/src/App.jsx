36930c0b177f2e21affabe3943a1f55c57ad3b55
Williams-Ignacio <ignacio.williams@alu.frlp.utn.edu.ar>
2025-11-20 17:36:31 -0300

    rutas del frontend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,10 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//imports de cada pagina
+import Grupo from "./pages/grupo/Grupo.jsx";
+import Inventario from "./pages/inventario e infraestructura/Inventario.jsx";
+import Login from "./pages/login/Login.jsx";
+import Planificacion from "./pages/planificacion/Planificacion.jsx";
+import Proyecto from "./pages/proyectos/Proyecto.jsx";
+
+
 function App() {
-  return (
-    <h1>Prueba</h1>
-  )
+  	return (
+		<Router>
+			<Routes>
+				<Route path="/grupo" element={ <Grupo/> }></Route>
+				<Route path="/grupo/planificacion/inventario" element={ <Inventario/> }></Route>
+				<Route path="/login" element={ <Login/> }></Route>
+				<Route path="/grupo/planificacion" element={ <Planificacion/> }></Route>
+				<Route path="/grupo/planificacion/proyecto" element={ <Proyecto/> }></Route>
+			</Routes>
+		</Router>
+  	)
 }
 
 export default App
